744e8f7e8d5bb7db5fcfc09c6f7eafdd60ad0a04
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-01 16:22:21 -0300

    made some changes

--- src/app/components/global/header.tsx
+++ src/app/components/global/header.tsx
@@ -19,14 +19,14 @@ export default function Header() {
         return (
             <div className='w-full h-16 flex flex-row justify-between items-center p-2'>
                 <div className="ml-2 p-0.5 w-8 rounded-full border-2 border-neutral-950 aspect-square flex justify-center items-center">
-                    <VercelLogo className="fill-neutral-950" />
+                    <VercelLogo className="fill-neutral-950 aspect-square w-6" />
                 </div>
-                <div className="flex flex-row">
+                <div className="flex flex-row items-center">
                     <nav className='flex flex-row'>
                         <Link href="/dashboard" className='text-neutral-950 hover:text-blue-400 mx-2'>Dashboard</Link>
                         <Link href="/login" className='text-neutral-950 hover:text-blue-400 mx-2'>Cadastrar</Link>
                     </nav>
-                    <BellIcon className="aspect-square w-8 mr-2 ml-4" />
+                    <BellIcon className="aspect-square w-6 mr-2 ml-4" />
                 </div>
             </div>
         );

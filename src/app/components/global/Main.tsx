4e1f3fdcf70a7acb72e64190da405f2b9a2980f4
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-02 16:30:40 -0300

    made some changes

--- src/app/components/global/Main.tsx
+++ src/app/components/global/Main.tsx
@@ -15,19 +15,19 @@ export default function Main({ children }: any) {
     return (
         <div className="w-full h-full relative">
             <Header showNotifications={handleShowNotifications} />
-            <div className={(showNotification ? "block" : "hidden") + " w-screen h-screen bg-transparent z-10 absolute top-0 left-0 bg-neutral-950/50"}>
+            <div className={(showNotification ? "block" : "hidden") + " w-full h-full bg-transparent z-10 absolute top-20 left-0 bg-neutral-950/50"}>
                 <div className="w-full h-full relative">
-                    <div className="bg-neutral-50 drop-shadow-lg rounded-md w-96 m-4 absolute top-6 right-6 last:border-none">
-                        <div className="border-b-[1px] border-neutral-400 w-full p-2 w-80">
+                    <div className="bg-neutral-50 drop-shadow-lg rounded-md w-96 m-4 absolute top-0 right-8 last:border-none">
+                        <div className="border-neutral-400 w-full p-2 w-80">
                             <h1>Notificação #00</h1>
                         </div>
-                        <div className="border-b-[1px] border-neutral-400 w-full p-2 w-80">
+                        <div className="border-neutral-400 w-full p-2 w-80">
                             <h1>Notificação #01</h1>
                         </div>
-                        <div className="border-b-[1px] border-neutral-400 w-full p-2 w-80">
+                        <div className="border-neutral-400 w-full p-2 w-80">
                             <h1>Notificação #02</h1>
                         </div>
-                        <div className="border-b-[1px] border-neutral-400 w-full p-2 w-80">
+                        <div className="border-neutral-400 w-full p-2 w-80">
                             <h1>Notificação #03</h1>
                         </div>
                     </div>

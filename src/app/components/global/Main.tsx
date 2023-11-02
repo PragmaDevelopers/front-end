cbc478253634b6edc56f3133fbd93e2e28c143a5
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-02 17:43:42 -0300

    made some changes

--- src/app/components/global/Main.tsx
+++ src/app/components/global/Main.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Header from "./header";
+import Link from "next/link";
 
 
 
@@ -18,18 +19,21 @@ export default function Main({ children }: any) {
             <div className={(showNotification ? "block" : "hidden") + " w-full h-full bg-transparent z-10 absolute top-20 left-0 bg-neutral-950/50"}>
                 <div className="w-full h-full relative">
                     <div className="bg-neutral-50 drop-shadow-lg rounded-md w-96 m-4 absolute top-0 right-8 last:border-none">
-                        <div className="border-neutral-400 w-full p-2 w-80">
-                            <h1>Notificação #00</h1>
+                        <div className="p-2 last:border-none odd:border-b-2 odd:border-neutral-400 even:border-b-2 even:border-neutral-400">
+                            <div className="h-full p-2">
+                                <h1>Notificação #00</h1>
+                            </div>
+                            <div className="w-full p-2">
+                                <h1>Notificação #01</h1>
+                            </div>
+                            <div className="w-full p-2">
+                                <h1>Notificação #02</h1>
+                            </div>
+                            <div className="w-full p-2">
+                                <h1>Notificação #03</h1>
+                            </div>
                         </div>
-                        <div className="border-neutral-400 w-full p-2 w-80">
-                            <h1>Notificação #01</h1>
-                        </div>
-                        <div className="border-neutral-400 w-full p-2 w-80">
-                            <h1>Notificação #02</h1>
-                        </div>
-                        <div className="border-neutral-400 w-full p-2 w-80">
-                            <h1>Notificação #03</h1>
-                        </div>
+                        <Link href="/dashboard/notifications" className="text-blue-300 hover:text-blue-500 transition-all">Ver todas as notificações</Link>
                     </div>
                 </div>
             </div>

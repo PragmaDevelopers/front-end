f121408c0988c13b72ab05b488e060683e5e3ba3
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-02 17:53:27 -0300

    made some changes

--- src/app/components/global/Main.tsx
+++ src/app/components/global/Main.tsx
@@ -16,25 +16,23 @@ export default function Main({ children }: any) {
     return (
         <div className="w-full h-full relative">
             <Header showNotifications={handleShowNotifications} />
-            <div className={(showNotification ? "block" : "hidden") + " w-full h-full bg-transparent z-10 absolute top-20 left-0 bg-neutral-950/50"}>
-                <div className="w-full h-full relative">
-                    <div className="bg-neutral-50 drop-shadow-lg rounded-md w-96 m-4 absolute top-0 right-8 last:border-none">
-                        <div className="p-2 last:border-none odd:border-b-2 odd:border-neutral-400 even:border-b-2 even:border-neutral-400">
-                            <div className="h-full p-2">
-                                <h1>Notificação #00</h1>
-                            </div>
-                            <div className="w-full p-2">
-                                <h1>Notificação #01</h1>
-                            </div>
-                            <div className="w-full p-2">
-                                <h1>Notificação #02</h1>
-                            </div>
-                            <div className="w-full p-2">
-                                <h1>Notificação #03</h1>
-                            </div>
+            <div className={(showNotification ? "block" : "hidden") + " w-full h-full bg-transparent z-10 absolute top-[4.5rem] left-0 bg-neutral-950/50 relative"}>
+                <div className="bg-neutral-50 drop-shadow-lg rounded-md w-64 m-4 absolute top-0 right-4">
+                    <div className="p-2">
+                        <div className="h-full p-2 border-2 rounded-md border-neutral-400 my-2">
+                            <h1>Notificação #00</h1>
                         </div>
-                        <Link href="/dashboard/notifications" className="text-blue-300 hover:text-blue-500 transition-all">Ver todas as notificações</Link>
+                        <div className="w-full p-2 border-2 rounded-md border-neutral-400 my-2">
+                            <h1>Notificação #01</h1>
+                        </div>
+                        <div className="w-full p-2 border-2 rounded-md border-neutral-400 my-2">
+                            <h1>Notificação #02</h1>
+                        </div>
+                        <div className="w-full p-2 border-2 rounded-md border-neutral-400 my-2">
+                            <h1>Notificação #03</h1>
+                        </div>
                     </div>
+                    <Link href="/dashboard/notifications" className="text-blue-400 hover:text-blue-500 transition-all">Ver todas as notificações</Link>
                 </div>
             </div>
             <div className='w-full h-full overflow-hidden'>

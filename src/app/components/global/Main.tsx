a673b55dc748a072d8ff6e0ce22fbfdb3e31c272
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-02 18:23:49 -0300

    made some changes

--- src/app/components/global/Main.tsx
+++ src/app/components/global/Main.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Header from "./header";
 import Link from "next/link";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 
 
@@ -19,7 +20,8 @@ export default function Main({ children }: any) {
             <div className={(showNotification ? "block" : "hidden") + " w-full h-full bg-transparent z-10 absolute top-[4.5rem] left-0 bg-neutral-950/50"}>
                 <div className="bg-transparent w-full h-full relative">
                     <div className="bg-neutral-50 drop-shadow-lg rounded-md w-64 m-4 absolute top-0 right-4 p-2">
-                        <div className="divide-y divide-neutral-200">
+                        <h1 className="font-bold text-lg">Notificações</h1>
+                        <div className="divide-y divide-neutral-200 my-4">
                             <div className="h-full p-2">
                                 <h1>Notificação #00</h1>
                             </div>
@@ -33,7 +35,7 @@ export default function Main({ children }: any) {
                                 <h1>Notificação #03</h1>
                             </div>
                         </div>
-                        <Link href="/dashboard/notifications" className="text-blue-400 hover:text-blue-500 transition-all">Ver todas as notificações</Link>
+                        <Link href="/dashboard/notifications" className="ml-0 hover:ml-2 text-sm fill-blue-400 hover:fill-blue-500 text-blue-400 hover:text-blue-500 transition-all">Ver todas as notificações <ArrowRightIcon className="w-4" /></Link>
                     </div>
                 </div>
             </div>
--- src/app/dashboard/notifications/page.tsx
+++ src/app/dashboard/notifications/page.tsx
@@ -2,8 +2,16 @@
 
 export default function Page() {
     return (
-        <main>
-            <h1>Notificações</h1>
+        <main className="w-full h-full flex flex-col">
+            <h1 className="font-bold text-xl">Notificações</h1>
+            <div className="w-full h-full flex flex-row justify-between items-center">
+                <div className="h-full m-4 drop-shadow-lg bg-neutral-50 rounded-md w-96">
+                    <h1>Menu</h1>
+                </div>
+                <div className="w-full h-full m-4 drop-shadow-lg bg-neutral-50 rounded-md">
+                    <h1>Item</h1>
+                </div>
+            </div>
         </main>
     );
 }

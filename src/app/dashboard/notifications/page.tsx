6d5407b90a2f92afca7c26e513efa144805ab8f1
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-02 19:40:49 -0300

    made some changes

--- src/app/dashboard/notifications/page.tsx
+++ src/app/dashboard/notifications/page.tsx
@@ -25,21 +25,21 @@ export default function Page() {
             <h1 className="font-bold text-xl mt-4">Notificações</h1>
             <div className="w-full h-full flex flex-row justify-start items-start">
                 <div className="h-[88%] ml-4 mr-4 mt-4 mb-8 shadow-inner border-[1px] border-neutral-300 bg-neutral-200 rounded-md w-[20%]">
-                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all my-2">
+                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all my-2 flex flex-row">
                         <TagIcon className="w-6 aspect-square ml-4" />
                         <h1>Notificações PUSH</h1>
                     </div>
-                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all mt-4">
+                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all mt-4 flex flex-row">
                         <TagIcon className="w-6 aspect-square ml-4" />
-                        <h1>Movimentações de Processos</h1>
+                        <h1 className="text-sm font-semibold">Movimentações de Processos</h1>
                     </div>
-                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all mt-4">
+                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all mt-4 flex flex-row">
                         <TagIcon className="w-6 aspect-square ml-4" />
-                        <h1>Prazos Expirados</h1>
+                        <h1 className="text-sm font-semibold">Prazos Expirados</h1>
                     </div>
-                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all mt-4">
+                    <div className="w-full h-8 ml-0 hover:ml-2 transition-all mt-4 flex flex-row">
                         <TagIcon className="w-6 aspect-square ml-4" />
-                        <h1>Ações do Sistema</h1>
+                        <h1 className="text-sm font-semibold">Ações do Sistema</h1>
                     </div>
                 </div>
                 <div className="w-[75%] h-[88%] shadow-inner border-[1px] border-neutral-300 bg-neutral-200 ml-4 mr-4 mt-4 mb-8 rounded-md divide-y divide-neutral-400 overflow-auto">

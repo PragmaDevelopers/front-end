68589ef6e1e95fd8c09f031f628f62ada2acc5ea
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-18 11:41:39 -0300

    made some changes

---
+++ src/app/dashboard/config/admin/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Page() {
+    return (
+        <main className="w-full h-full bg-neutral-100 overflow-hidden">
+            <h1>Admin</h1>
+            <div>
+
+            </div>
+        </main>
+    );
+}
+

e39f87fc69b629781c37bcf702a75a2d2118499c
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-18 11:39:46 -0300

    made some changes

--- src/app/dashboard/board/[id]/page.tsx
+++ src/app/dashboard/board/[id]/page.tsx
@@ -84,7 +84,7 @@ import 'react-calendar/dist/Calendar.css';
 function ConfirmDelete(props: ConfirmDeleteProps) {
     return (
         <div className={(props.showPrompt ? 'block' : 'hidden') + ' absolute z-50 top-0 left-0 w-screen h-screen flex justify-center items-center'}>
-            <div className='bg-neutral-50 drop-shadow-lg rounded-md p-4 flex justify-center items-center'>
+            <div className='bg-neutral-50 drop-shadow-lg rounded-md p-4 flex justify-center items-center flex-col'>
                 <h1 className='mb-4'>{props.message}</h1>
                 <div className='m-2'>
                     <button type="button" onClick={props.yesFunction} className='mx-2 p-2 rounded-md border-neutral-950 border-2 bg-neutral-50 text-neutral-950 hover:bg-neutral-950 hover:text-neutral-50 transition-all'>{props.yesText}</button>

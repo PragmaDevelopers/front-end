e9c5716481048c8d6ec63feff7e959415df8f752
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-18 15:58:58 -0300

    made some changes

--- src/app/components/ui/SwitchButton.tsx
+++ src/app/components/ui/SwitchButton.tsx
@@ -29,15 +29,14 @@ export default function SwitchButton(props: SwitchButtonProps) {
             checked={enabled}
             onChange={handleSwitch}
             className={`${enabled ? 'bg-red-100' : 'bg-green-200'
-                } relative inline-flex h-6 w-11 items-center rounded-full`}
+                } relative inline-flex h-6 w-11 items-center rounded-full shadow-inner border-[1px] border-neutral-200`}
         >
             <span className="sr-only">{srText}</span>
             <span
                 className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-                    } inline-block h-4 w-4 transform rounded-full bg-white transition flex justify-center items-center`}>
+                    } inline-block h-4 w-4 transform rounded-full bg-white transition flex justify-center items-center drop-shadow-sm`}>
                 <CheckIcon className={`${enabled ? 'hidden' : 'block'} h-3 w-3 aspect-square full-green-500 stroke-green-500`} />
                 <XMarkIcon className={`${enabled ? 'block' : 'hidden'} h-3 w-3 aspect-square full-red-500 stroke-red-500`} />
-
             </span>
         </Switch>
     );

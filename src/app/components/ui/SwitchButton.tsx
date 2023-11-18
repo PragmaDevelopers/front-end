531b1b496c28ee6c08745950a44373921d6d86d2
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-18 15:33:56 -0300

    made some changes

--- src/app/components/ui/SwitchButton.tsx
+++ src/app/components/ui/SwitchButton.tsx
@@ -1,24 +1,44 @@
 "use client";
 
 import { Switch } from "@headlessui/react";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 
-export default function SwitchButton() {
+interface SwitchButtonProps {
+    srText: string;
+    onFunction: any;
+    offFunction: any;
+}
+
+export default function SwitchButton(props: SwitchButtonProps) {
     const [enabled, setEnabled] = useState<boolean>(false);
 
+    const { srText, onFunction, offFunction } = props;
+
+    const handleSwitch = () => {
+        setEnabled(!enabled);
+        if (enabled) {
+            onFunction();
+        } else {
+            offFunction();
+        }
+    }
 
     return (
         <Switch
             checked={enabled}
-            onChange={setEnabled}
-            className={`${enabled ? 'bg-green-600' : 'bg-red-200'
+            onChange={handleSwitch}
+            className={`${enabled ? 'bg-green-200' : 'bg-red-100'
                 } relative inline-flex h-6 w-11 items-center rounded-full`}
         >
-            <span className="sr-only">Enable notifications</span>
+            <span className="sr-only">{srText}</span>
             <span
                 className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-                    } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-            />
+                    } inline-block h-4 w-4 transform rounded-full bg-white transition flex justify-center items-center`}>
+                <CheckIcon className={`${enabled ? 'block' : 'hidden'} h-3 w-3 aspect-square full-green-500 stroke-green-500`} />
+                <XMarkIcon className={`${enabled ? 'hidden' : 'block'} h-3 w-3 aspect-square full-red-500 stroke-red-500`} />
+
+            </span>
         </Switch>
     );
 }

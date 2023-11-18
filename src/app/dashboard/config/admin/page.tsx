f1ef4f5f2f3a1f08d1cd8f565ad44f2143a01516
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-18 16:10:12 -0300

    made some changes

--- src/app/dashboard/config/admin/page.tsx
+++ src/app/dashboard/config/admin/page.tsx
@@ -4,9 +4,26 @@ import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
+interface ToggleOptionProps {
+    optionText: string;
+    srText: string;
+    onFunction: any;
+    offFunction: any;
+}
+
+function ToggleOption(props: ToggleOptionProps) {
+    const { optionText, offFunction, onFunction, srText } = props;
+
+    return (
+        <div className="flex justify-between items-center w-fit h-fit">
+            <p className="ml-4">{optionText}</p>
+            <SwitchButton srText={srText} onFunction={onFunction} offFunction={offFunction} />
+        </div>
+    );
+}
+
 export default function Page() {
     const router = useRouter();
-    const [text, setText] = useState<string>("");
     return (
         <main className="w-full h-full bg-neutral-100 overflow-hidden">
             <div className="flex flex-row justify-center items-center relative my-2">
@@ -14,8 +31,9 @@ export default function Page() {
                 <h1 className="font-bold text-2xl text-neutral-900">Admin</h1>
             </div>
             <div className="mt-4 flex flex-col items-center justify-between relative p-4">
-                <SwitchButton srText="Alternar Permissão" onFunction={() => setText("ON")} offFunction={() => setText("OFF")} />
-                <p>{text}</p>
+                <div className="w-fit h-fit p-2 bg-neutral-50 drop-shadow-md rounded-md">
+                    <ToggleOption optionText="Criar Cards" srText="Alternar Permissão" onFunction={() => console.log("ON")} offFunction={() => console.log("OFF")} />
+                </div>
             </div>
         </main>
     );

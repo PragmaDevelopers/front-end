1604939c2ff4f19b0b56b4da731a7974c9d05a6f
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-18 16:31:24 -0300

    made some changes

--- src/app/dashboard/config/admin/page.tsx
+++ src/app/dashboard/config/admin/page.tsx
@@ -15,7 +15,7 @@ function ToggleOption(props: ToggleOptionProps) {
     const { optionText, offFunction, onFunction, srText, className } = props;
 
     return (
-        <div className={className + " my-2 flex justify-between items-center w-fit h-fit"}>
+        <div className={className + " my-1 flex justify-between items-center w-fit h-fit"}>
             <p className="mr-4">{optionText}</p>
             <SwitchButton srText={srText} onFunction={onFunction} offFunction={offFunction} />
         </div>
@@ -30,8 +30,8 @@ export default function Page() {
                 <button className="absolute left-4 hover:left-2 transition-all" type="button" onClick={() => router.back()}><ArrowLeftIcon className="aspect-square w-8 stroke-1 stroke-neutral-900 fill-neutral-900" /></button>
                 <h1 className="font-bold text-2xl text-neutral-900">Admin</h1>
             </div>
-            <div className="mt-4 flex flex-col items-center justify-center relative p-4 w-full h-full overflow-hidden">
-                <div className="w-fit h-fit p-2 bg-neutral-50 drop-shadow-md rounded-md w-[90%] flex flex-col justify-start items-end overflow-auto">
+            <div className="mt-4 flex flex-col items-center justify-start relative p-4 w-full h-full overflow-hidden">
+                <div className="p-2 bg-neutral-50 drop-shadow-md rounded-md w-[90%] flex flex-col justify-start items-end overflow-auto">
                     <ToggleOption optionText="Criar Cards" srText="Alternar Permissão"
                         onFunction={() => console.log("Criar Cards ON")} offFunction={() => console.log("Criar Cards OFF")} />
                     <ToggleOption optionText="Mover Cards" srText="Alternar Permissão"
@@ -39,7 +39,7 @@ export default function Page() {
                     <ToggleOption optionText="Deletar Cards" srText="Alternar Permissão"
                         onFunction={() => console.log("Deletar Cards ON")} offFunction={() => console.log("Deletar Cards OFF")} />
                 </div>
-                <button type="button" className="m-2 p-2 bg-neutral-50 drop-shadow-md rounded-md text-green-600 hover:bg-green-600 hover:text-neutral-50 hover:scale-110 transition-all">
+                <button type="button" className="mt-4 p-2 bg-neutral-50 drop-shadow-md rounded-md text-green-600 hover:bg-green-600 hover:text-neutral-50 hover:scale-110 transition-all">
                     Salvar
                 </button>
             </div>

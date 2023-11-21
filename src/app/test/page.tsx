00b08906f2f08c8b37400a1617b6e0847b4ce805
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-11-21 16:04:56 -0300

    made some changes

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { Component } from "react";
+import { useState } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import 'dayjs/locale/pt-br';
 import { UserIcon } from "@heroicons/react/24/outline";
+import { DateValue } from "../types/KanbanTypes";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 dayjs.locale('pt-br');
 dayjs.extend(relativeTime);
@@ -65,9 +68,65 @@ function CommentSection() {
 }
 
 export default function Page() {
+    const [cardDate, setCardDate] = useState<DateValue>(new Date());
+    const [file, setFile] = useState(null);
+    const [url, setUrl] = useState('');
+
+    const handleSetCardDate = (value: any) => {
+        setCardDate(value);
+        console.log(value);
+    }
+
+    const handleFileChange = (e: any) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleUrlChange = (e: any) => {
+        setUrl(e.target.value);
+    };
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        // Handle file or URL submission
+        if (file) {
+            // Upload file
+            const formData = new FormData();
+            formData.append('file', file);
+
+            // // Send formData to the API route
+            // const response = await fetch('/api/upload', {
+            //     method: 'POST',
+            //     body: formData,
+            // });
+
+            // const result = await response.json();
+            // console.log(result);
+            console.log(formData);
+        } else if (url) {
+            // // Handle URL submission
+            // const response = await fetch('/api/upload?url=' + encodeURIComponent(url));
+            // const result = await response.json();
+            // console.log(result);
+            console.log(encodeURIComponent(url));
+        }
+    };
+
     return (
         <main className="w-full h-full bg-neutral-50">
             <CommentSection />
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type="file" onChange={handleFileChange} />
+                    <br />
+                    <input type="text" placeholder="Enter URL" value={url} onChange={handleUrlChange} />
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <div>
+                <Calendar value={cardDate} onChange={handleSetCardDate} />
+            </div>
         </main>
     );
 }

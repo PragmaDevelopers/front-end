38806d9a06974802a9ca65e9ce6966624491ed54
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-12-24 19:36:18 -0300

    made some changes

--- src/app/utils/dashboard/functions/CreateEditCard.ts
+++ src/app/utils/dashboard/functions/CreateEditCard.ts
@@ -1,4 +1,4 @@
-function handleShowTag() {
+export function handleShowTag() {
     if (isFlagSet(userValue.userData, "CRIAR_TAG")) {
         setViewAddTag(!viewAddTag)
     } else {
@@ -16,6 +16,119 @@ function handleShowTag() {
             (el: CustomModalButtonAttributes, idx: number) => <button className={ el?.className } type = { el.type } key = { idx } onClick = { el.onclickfunc } ref = { el?.ref } > { el.text } < /button>);
 
     setModalTitle("Ação Negada.");
+        setModalDescription("Você não tem as permissões necessárias para realizar esta ação.");
+        setModalText("Fale com seu administrador se isto é um engano.");
+        setModalBorderColor("border-red-500");
+        setModalFocusRef(noButtonRef);
+        setModalOptions(modalOpt);
+        setModalOpen(true);
+        return;
+    }
+};
+
+
+export function handleShowDate() {
+    if (isFlagSet(userValue.userData, "CRIAR_PRAZOS")) {
+        setViewAddDate(!viewAddDate)
+    } else {
+        const optAttrs: CustomModalButtonAttributes[] = [
+            {
+                text: "Entendido.",
+                onclickfunc: () => setModalOpen(false),
+                ref: noButtonRef,
+                type: "button",
+                className: "rounded-md border border-transparent bg-neutral-100 px-4 py-2 text-sm font-medium text-neutral-900 hover:bg-neutral-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-2"
+            }
+        ];
+
+        const modalOpt: any = optAttrs.map(
+            (el: CustomModalButtonAttributes, idx: number) => <button className={ el?.className } type = { el.type } key = { idx } onClick = { el.onclickfunc } ref = { el?.ref } > { el.text } < /button>);
+
+        setModalTitle("Ação Negada.");
+        setModalDescription("Você não tem as permissões necessárias para realizar esta ação.");
+        setModalText("Fale com seu administrador se isto é um engano.");
+        setModalBorderColor("border-red-500");
+        setModalFocusRef(noButtonRef);
+        setModalOptions(modalOpt);
+        setModalOpen(true);
+        return;
+    }
+};
+
+export function handleShowField() {
+    if (isFlagSet(userValue.userData, "CRIAR_CAMPO")) {
+        setViewAddField(!viewAddField)
+    } else {
+        const optAttrs: CustomModalButtonAttributes[] = [
+            {
+                text: "Entendido.",
+                onclickfunc: () => setModalOpen(false),
+                ref: noButtonRef,
+                type: "button",
+                className: "rounded-md border border-transparent bg-neutral-100 px-4 py-2 text-sm font-medium text-neutral-900 hover:bg-neutral-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-2"
+            }
+        ];
+
+        const modalOpt: any = optAttrs.map(
+            (el: CustomModalButtonAttributes, idx: number) => <button className={ el?.className } type = { el.type } key = { idx } onClick = { el.onclickfunc } ref = { el?.ref } > { el.text } < /button>);
+
+        setModalTitle("Ação Negada.");
+        setModalDescription("Você não tem as permissões necessárias para realizar esta ação.");
+        setModalText("Fale com seu administrador se isto é um engano.");
+        setModalBorderColor("border-red-500");
+        setModalFocusRef(noButtonRef);
+        setModalOptions(modalOpt);
+        setModalOpen(true);
+        return;
+    }
+};
+
+export function handleShowMember() {
+    if (isFlagSet(userValue.userData, "CONVIDAR_PARA_O_KANBAN")) {
+        setViewAddMember(!viewAddMember)
+    } else {
+        const optAttrs: CustomModalButtonAttributes[] = [
+            {
+                text: "Entendido.",
+                onclickfunc: () => setModalOpen(false),
+                ref: noButtonRef,
+                type: "button",
+                className: "rounded-md border border-transparent bg-neutral-100 px-4 py-2 text-sm font-medium text-neutral-900 hover:bg-neutral-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-2"
+            }
+        ];
+
+        const modalOpt: any = optAttrs.map(
+            (el: CustomModalButtonAttributes, idx: number) => <button className={ el?.className } type = { el.type } key = { idx } onClick = { el.onclickfunc } ref = { el?.ref } > { el.text } < /button>);
+
+        setModalTitle("Ação Negada.");
+        setModalDescription("Você não tem as permissões necessárias para realizar esta ação.");
+        setModalText("Fale com seu administrador se isto é um engano.");
+        setModalBorderColor("border-red-500");
+        setModalFocusRef(noButtonRef);
+        setModalOptions(modalOpt);
+        setModalOpen(true);
+        return;
+    }
+};
+
+export function handleShowMoveCard() {
+    if (isFlagSet(userValue.userData, "MOVER_CARDS")) {
+        setViewMoveCard(!viewMoveCard)
+    } else {
+        const optAttrs: CustomModalButtonAttributes[] = [
+            {
+                text: "Entendido.",
+                onclickfunc: () => setModalOpen(false),
+                ref: noButtonRef,
+                type: "button",
+                className: "rounded-md border border-transparent bg-neutral-100 px-4 py-2 text-sm font-medium text-neutral-900 hover:bg-neutral-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-2"
+            }
+        ];
+
+        const modalOpt: any = optAttrs.map(
+            (el: CustomModalButtonAttributes, idx: number) => <button className={ el?.className } type = { el.type } key = { idx } onClick = { el.onclickfunc } ref = { el?.ref } > { el.text } < /button>);
+
+        setModalTitle("Ação Negada.");
         setModalDescription("Você não tem as permissões necessárias para realizar esta ação.");
         setModalText("Fale com seu administrador se isto é um engano.");
         setModalBorderColor("border-red-500");

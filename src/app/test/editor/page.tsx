4947c5c5c9a0fb58c5f7679733d2395ec0b93800
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2024-01-04 17:29:25 -0300

    made some changes

--- src/app/test/editor/page.tsx
+++ src/app/test/editor/page.tsx
@@ -2,12 +2,11 @@
 
 import { MDXEditor, headingsPlugin, listsPlugin, quotePlugin, thematicBreakPlugin, linkPlugin, linkDialogPlugin, tablePlugin, markdownShortcutPlugin, toolbarPlugin, UndoRedo, BlockTypeSelect, BoldItalicUnderlineToggles, InsertTable, ListsToggle, CreateLink } from "@mdxeditor/editor";
 import '@mdxeditor/editor/style.css';
+import { forwardRef } from "react";
 
-export default function Page() {
+const Editor = forwardRef(() => {
     return (
-        <main className="w-full h-full">
-            <div className="w-[75%] h-[75%]">
-                <MDXEditor
+            <MDXEditor
             className="MDXEditor"
             onChange={console.log}
             markdown={""}
@@ -35,6 +34,17 @@ export default function Page() {
             ]}
 
         />
+
+    );
+});
+
+Editor.displayName = "Editor";
+
+export default function Page() {
+    return (
+        <main className="w-full h-full">
+            <div className="w-[75%] h-[75%]">
+                <Editor/>
             </div>
         </main>
     );

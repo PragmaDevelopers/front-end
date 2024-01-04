c357687d00df236268cdeb8e3fe086749baa4fbd
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2024-01-04 13:06:35 -0300

    made some changes

--- src/app/dashboard/config/board/[id]/page.tsx
+++ src/app/dashboard/config/board/[id]/page.tsx
@@ -5,11 +5,11 @@
  *  [x] Remove Members
  *  [ ] Tags
  *  [ ] Custom Fields
- *  [ ] Renomear Kanban
+ *  [x] Renomear Kanban
  * */
 
 import { useUserContext } from "@/app/contexts/userContext";
-import { Member, SystemID, userData } from "@/app/types/KanbanTypes";
+import { CustomFieldsTemplate, Member, SystemID, userData } from "@/app/types/KanbanTypes";
 import { API_BASE_URL } from "@/app/utils/variables";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
@@ -31,6 +31,83 @@ function RenameKanbanSection(props: RenameKanbanSectionProps) {
             <button type="submit" className={submitButtonStyles}>{submitButtonText}</button>
         </form>
     );
+}
+
+interface RemoveCustomFieldTemplateProps {
+    setSelectedTemplates: any;
+    selectedTemplates: CustomFieldsTemplate[];
+    templates: CustomFieldsTemplate[];
+}
+function RemoveCustomFieldTemplateSection(props: RemoveCustomFieldTemplateProps) {
+    const { setSelectedTemplates, selectedTemplates, templates } = props;    
+    const [query, setQuery] = useState('');
+
+    return (
+    <Combobox value={selectedTemplates} onChange={setSelectedTemplates} multiple>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                displayValue={(templates: CustomFieldsTemplate[]) =>
+                    templates.map((item: CustomFieldsTemplate) => item.name).join(', ')
+                }
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {
+                templates.map((value: any, index: number) => (
+                  <Combobox.Option
+                    key={index}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={value}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {value.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              }
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    );
+
 }
 
 

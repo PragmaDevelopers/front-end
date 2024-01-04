c1802ba0d05ca06004876ec211aef3cbcbebfe85
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2024-01-04 15:11:05 -0300

    made some changes

--- src/app/dashboard/config/board/[id]/page.tsx
+++ src/app/dashboard/config/board/[id]/page.tsx
@@ -16,11 +16,92 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState, Fragment } from "react";
 import { Combobox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import { HexColorPicker } from "react-colorful";
 
+interface SelectTagsSectionProps {
+    tags: Tag[],
+    setSelected: any;
+    selected: Tag | undefined,
+}
+function SelectTagsSection(props: SelectTagsSectionProps) {
+    const { selected, setSelected, tags } = props;
 
-function SelectTagsSection() {
+    const [query, setQuery] = useState('')
+
+    const filteredTags =
+        query === ''
+        ? tags
+        : tags.filter((tag) =>
+            tag.name.toLowerCase().replace(/\s+/g, '').includes(query.toLowerCase().replace(/\s+/g, ''))
+        )
+
     return (
-        
+        <Combobox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              displayValue={(tag: Tag) => tag.name}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {tags.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredTags.map((tag: Tag) => (
+                  <Combobox.Option
+                    key={tag.id}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={tag}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {tag.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
     );
 }
 
@@ -294,7 +375,11 @@ export default function Page({ params }: { params: { id: string } }) {
     const [removeSelectedUsers, setRemoveSelectedUsers] = useState<Member[]>([]);
     const [customFieldsTemplates, setCustomFieldsTemplates] = useState<CustomFieldsTemplate[]>([]);
     const [selectedCustomFieldsTemplates, setSelectedCustomFieldsTemplates] = useState<CustomFieldsTemplate[]>([]);
-    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+    const [allTags, setAllTags] = useState<Tag[]>([]);
+    const [selectedTag, setSelectedTag] = useState<Tag>();
+    const [isEditingTag, setIsEditingTag] = useState<boolean>(false);
+    const [color, setColor] = useState<string>("");
+    const [tagNameDefaultValue, setTagNameDefaultValue] = useState<string>("");
 
 
     useEffect(() => {
@@ -347,8 +432,22 @@ export default function Page({ params }: { params: { id: string } }) {
 
 
 
+        const tagsRequest = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userValue.token}`,
+            },
+        }
+        fetch(`${API_BASE_URL}/api/private/user/kanban/column/card/tags`, 
+            tagsRequest).then(response => response.json()).then((data: Tag[]) => {
+                console.log(data);
+                setAllTags(data);
+        })
 
-    }, [userValue, params, setKanbanTitle, setUsersArray, setAllUsersArray, setCustomFieldsTemplates]);
+
+
+    }, [userValue, params, setKanbanTitle, setUsersArray, setAllUsersArray, setCustomFieldsTemplates, setAllTags]);
 
     const updateUsersArray = () => {
         const usersRequestOptions = {
@@ -429,6 +528,40 @@ export default function Page({ params }: { params: { id: string } }) {
     }
 
 
+    const handleDeleteTag = () => {
+        const requestOpts = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userValue.token}`,
+            },
+        }
+
+        fetch(`${API_BASE_URL}/api/private/user/kanban/column/card/tag/${selectedTag?.id}`, requestOpts);
+        console.log(`Tag ${selectedTag?.name} removida.`);
+    }
+
+    const handleEditTag = () => {
+        setColor(selectedTag?.color as string);
+        setTagNameDefaultValue(selectedTag?.name as string);
+        setIsEditingTag(true);
+    }
+
+    const handleEditTagSubmit = (e: any) => {
+        e.preventDefault();
+        let tagTitle: string = e.target.title.value;
+        let tagColor: string = color;
+        const requestOpts = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userValue.token}`,
+            },
+            body: JSON.stringify({name: tagTitle, color: tagColor}),
+        }
+        fetch(`${API_BASE_URL}/api/private/user/kanban/column/card/tag/${selectedTag?.id}`, requestOpts);
+    }
+
     return (
         <main className="w-full h-full overflow-auto shrink-0">
             <div className="relative w-full flex flex-row justify-center items-center px-2 my-2">
@@ -498,7 +631,33 @@ export default function Page({ params }: { params: { id: string } }) {
 
 
             <div>
+                <div className={`${isEditingTag ? 'flex' : 'hidden'} absolute inset-0 w-full h-full justify-center items-center bg-neutral-950/25`}>
+                    <div className="w-fit h-fit p-2 rounded-md shadow-md">
+                        <button type="button" onClick={() => setIsEditingTag(false)}>
+                            <XCircleIcon className="w-6 aspect-square"/>
+                        </button>
+                        <form onSubmit={handleEditTagSubmit}>
+                            <input defaultValue={tagNameDefaultValue} type='text' name='title' placeholder='Nome da Etiqueta' className='form-input bg-neutral-100 w-48 border-[1px] border-neutral-200 rounded-md p-1 shadow-inner m-2' />
+                            <HexColorPicker color={color} onChange={setColor} className='m-2' />
+                            <button type='submit' className="bg-neutral-50 p-2 drop-shadow rounded-md my-2">Salvar</button>
+                        </form>
+                    </div>
+                </div>
 
+
+                <SelectTagsSection 
+                    tags={allTags}
+                    selected={selectedTag}
+                    setSelected={setSelectedTag}
+                />
+
+                <button type="button" onClick={handleEditTag}>
+                    editar etiqueta
+                </button>
+
+                <button type="button" onClick={handleDeleteTag}>
+                    remover etiqueta
+                </button>
             </div>
 
 

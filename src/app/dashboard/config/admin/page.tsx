2aee58c195562549c75a98d97d7927af88f73c58
Lucas Emanuel Santana Dos Santos <lucasemanuel2077@gmail.com>
2024-01-18 15:16:36 -0300

    new

--- src/app/dashboard/config/admin/page.tsx
+++ src/app/dashboard/config/admin/page.tsx
@@ -3,7 +3,7 @@ import SwitchButton from "@/app/components/ui/SwitchButton";
 import { useUserContext } from "@/app/contexts/userContext";
 import { User, userValueDT } from "@/app/types/KanbanTypes";
 import { SYSTEM_PERMISSIONS, SYSTEM_PERMISSIONS_BOOLEAN } from "@/app/utils/variables";
-import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@headlessui/react";
+import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon, InformationCircleIcon } from "@heroicons/react/24/outline";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
@@ -82,15 +82,16 @@ function updateSelectedUser(userValue: userValueDT, setUserValue: (newValue: use
 
 
 export default function Page() {
-  const router = useRouter();
   const { userValue, setUserValue } = useUserContext();
+  const [selectedMember,setSelectedMember] = useState<User>();
+  const [filteredUsers,setFilteredUsers] = useState<User[]>([]);
   const [usrPermsVal, setUsrPermsVal] = useState<{ [Key: string]: boolean }>({});
   const [isUserSupervisor, setIsUserSupervisor] = useState<boolean>(false);
-
   const [selected, setSelected] = useState<User>(userValue.userList[0]);
   const [query, setQuery] = useState('');
 
   const filteredPeople = query === '' ? userValue.userList : userValue.userList.filter((person: User) => person.name.toLowerCase().includes(query.toLowerCase()));
+  const router = useRouter();
 
   useEffect(() => {
     if (selected && selected.permissionLevel) {
@@ -112,7 +113,6 @@ export default function Page() {
       setSelected(value);
     }
   };
-  
 
   return (
     <main className="w-full h-full bg-neutral-100 overflow-hidden">
@@ -127,11 +127,67 @@ export default function Page() {
         <div className="w-[75%] flex flex-row justify-center items-center">
           <h1 className="mr-2">Editando permissões para o usuário: </h1>
           <div className="w-96 ml-2">
-            {/* <Combobox value={selected} onChange={handleSetSelect}>
-              <div className="relative mt-1">
-                
-              </div>
-            </Combobox> */}
+            <Combobox onChange={(user:User)=>setSelectedMember(user)}>
+                                <div className="relative mt-1">
+                                    <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                                        <Combobox.Input
+                                            className="form-input w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                                            displayValue={(user: User) => user.name}
+                                        />
+                                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                                            <ChevronUpDownIcon
+                                                className="h-5 w-5 text-gray-400"
+                                                aria-hidden="true"
+                                            />
+                                        </Combobox.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        leave="transition ease-in duration-100"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                        afterLeave={() => {}}
+                                    >
+                                        <Combobox.Options className="form-select absolute z-50 mt-1 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                                            {userValue.userList.length === 0 ? (
+                                                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                                                    Nothing found.
+                                                </div>
+                                            ) : (
+                                              userValue.userList.map((person: any) => (
+                                                    <Combobox.Option
+                                                        key={person.id}
+                                                        className={({ active }) =>
+                                                            `relative cursor-default select-none py-2 pl-10 pr-4 rounded-md ${active ? 'bg-teal-50 text-neutral-900' : 'text-gray-900'
+                                                            }`
+                                                        }
+                                                        value={person}
+                                                    >
+                                                        {({ selected, active }: any) => (
+                                                            <>
+                                                                <span
+                                                                    className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                                                        }`}
+                                                                >
+                                                                    {person.name}
+                                                                </span>
+                                                                {selected ? (
+                                                                    <span
+                                                                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-teal-600' : 'text-teal-600'
+                                                                            }`}
+                                                                    >
+                                                                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                    </span>
+                                                                ) : null}
+                                                            </>
+                                                        )}
+                                                    </Combobox.Option>
+                                                ))
+                                            )}
+                                        </Combobox.Options>
+                                    </Transition>
+                                </div>
+              </Combobox>
           </div>
         </div>
       </div>
